885dd14276d8da983bfd2d8efc9d5ff1e9bb0a6f
alinemorais <linianholete@gmail.com>
2024-08-06 22:03:23 -0300

    Adicionado exercicios sobre número inteiro (int)

---
+++ exercicios.py
@@ -0,0 +1,41 @@
+# %%
+# #### Inteiros (int)
+# %% 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+
+numero_01 = int(input('Digite um número inteiro: '))
+numero_02 = int(input('Digite outro número inteiro: '))
+
+resultado = numero_01 + numero_02
+
+resultado
+
+
+# %% 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+numero_01 = int(input('Digite um número inteiro: '))
+numero_02 = int(input('Digite outro número inteiro: '))
+
+resultado = numero_01 % numero_02
+
+resultado
+
+# %% 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+numero_01 = int(input('Digite um número inteiro: '))
+numero_02 = int(input('Digite outro número inteiro: '))
+
+resultado = numero_01 * numero_02
+
+resultado
+
+# %% 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+numero_01 = int(input('Digite um número inteiro: '))
+numero_02 = int(input('Digite outro número inteiro: '))
+
+resultado = numero_01 // numero_02
+
+resultado
+# %% 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.numero_01 = int(input('Digite um número inteiro: '))
+numero_02 = int(input('Digite outro número inteiro: '))
+
+resultado = numero_01 // numero_02
+
+resultado
