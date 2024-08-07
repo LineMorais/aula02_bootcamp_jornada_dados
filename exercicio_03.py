5eff9c328b5097c9e07b5e7c6e51bb662ee65e9a
alinemorais <linianholete@gmail.com>
2024-08-06 22:49:13 -0300

    Adicionados exercicios de tipo de dados

---
+++ exercicio_03.py
@@ -0,0 +1,11 @@
+# %% Strings (str)
+
+# %% 1. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
+
+# %% 2. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
+
+# %% 3. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
+
+# %% 4. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
+
+#  %% 5. Escreva um programa que concatene duas strings fornecidas pelo usuário.
--- exercicios.py
+++ exercicios.py
@@ -5,37 +5,36 @@
 numero_01 = int(input('Digite um número inteiro: '))
 numero_02 = int(input('Digite outro número inteiro: '))
 
-resultado = numero_01 + numero_02
+resultado_soma = numero_01 + numero_02
 
-resultado
-
+print(f"A soma é:", resultado_soma)
 
 # %% 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
 numero_01 = int(input('Digite um número inteiro: '))
-numero_02 = int(input('Digite outro número inteiro: '))
 
-resultado = numero_01 % numero_02
+resultado_resto = numero_01 % 5
 
-resultado
+print(f"O resto da divisão por 5 é:", resultado_resto)
 
 # %% 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
 numero_01 = int(input('Digite um número inteiro: '))
 numero_02 = int(input('Digite outro número inteiro: '))
 
-resultado = numero_01 * numero_02
+resultado_multiplicacao = numero_01 * numero_02
 
-resultado
+print(f"O resultado da multiplicação é:", resultado_multiplicacao)
 
 # %% 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 numero_01 = int(input('Digite um número inteiro: '))
 numero_02 = int(input('Digite outro número inteiro: '))
 
-resultado = numero_01 // numero_02
+resultado_divisao = numero_01 // numero_02
 
-resultado
-# %% 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.numero_01 = int(input('Digite um número inteiro: '))
+print(f"O resultado da divisão inteira é:", resultado_divisao)
+
+# %% 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário
 numero_02 = int(input('Digite outro número inteiro: '))
 
-resultado = numero_01 // numero_02
+resultado_quadrado = numero_01 ** 2
 
-resultado
+print(f"O quadrado do número é:", resultado_quadrado)
